d31f737b652532970b8c1cf97eb943e6bdf727c0
NEOWIZ\hgkim0905 <lacey0905@naver.com>
2019-07-24 18:20:57 +0900

    Start

---
+++ Assets/sample.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sample : MonoBehaviour
+{
+
+    public GameObject temp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        temp = GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
