c505cf059a32a746d0f24a38999ae322fbd58a21
NEOWIZ\hgkim0905 <lacey0905@naver.com>
2019-07-24 19:37:40 +0900

    ship

---
+++ Assets/Scripts/CShipController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CShipController : MonoBehaviour
+{
+
+    Rigidbody rigidbody;
+
+    public float shipSpeed = 10f;
+
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * shipSpeed * Time.smoothDeltaTime);
+
+        float h = Input.GetAxisRaw("horizontal");
+
+
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
