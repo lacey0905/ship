d2db1f31d47633b5a681f0490a783477b542ca37
DESKTOP-6QHKM6B\LACEY <lacey0905@naver.com>
2019-07-28 23:59:57 +0900

    Rotation

--- Assets/Scripts/CShipController.cs
+++ Assets/Scripts/CShipController.cs
@@ -8,7 +8,11 @@ public class CShipController : MonoBehaviour
     Rigidbody rigidbody;
 
     public float shipSpeed = 10f;
+    public float shipTurnSpeed = 100f;
 
+    public Vector3 rot;
+
+    public GameObject ship;
 
     private void Awake()
     {
@@ -17,17 +21,42 @@ public class CShipController : MonoBehaviour
 
     void Start()
     {
-        
     }
 
     private void Update()
     {
-        transform.Translate(Vector3.forward * shipSpeed * Time.smoothDeltaTime);
+        //transform.Translate(Vector3.forward * shipSpeed * Time.smoothDeltaTime);
 
-        float h = Input.GetAxisRaw("horizontal");
+        float h = Input.GetAxisRaw("Horizontal");
+
+
+        
+
+
+
+        if (h != 0)
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * h * shipTurnSpeed);
+            rot = this.transform.forward;
+        }
+        else
+        {
+            //Quaternion newRot = Quaternion.LookRotation(rot);
+
+            ship.transform.rotation = Quaternion.LookRotation(rot);
+
+            transform.rotation = ship.transform.rotation;
+        }
+
+
+
+
 
 
     }
+
+    
+
 
     void FixedUpdate()
     {
