7e9edb6760e72c36aabbfb6e51a7955939ea0110
NEOWIZ\hgkim0905 <lacey0905@naver.com>
2019-07-29 13:23:48 +0900

    회전

--- Assets/Scripts/CShipController.cs
+++ Assets/Scripts/CShipController.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class CShipController : MonoBehaviour
 {
 
+
+
     Rigidbody rigidbody;
 
     public float shipSpeed = 10f;
     public float shipTurnSpeed = 100f;
 
-    public Vector3 rot;
+    public float angle = 0f;
 
-    public GameObject ship;
+    public CShipRotate rotate;
 
-    private void Awake()
+    void Awake()
     {
         rigidbody = GetComponent<Rigidbody>();
     }
@@ -23,30 +25,47 @@ public class CShipController : MonoBehaviour
     {
     }
 
-    private void Update()
+    void Update()
     {
         //transform.Translate(Vector3.forward * shipSpeed * Time.smoothDeltaTime);
 
         float h = Input.GetAxisRaw("Horizontal");
 
 
+        if (h != 0)
+        {
+            angle += Time.deltaTime * h * shipTurnSpeed;
+            
+            if(angle > 360f)
+            {
+                angle = 0f;
+            }
+            Vector3 rot = new Vector3(0f, angle, 0f);
+
+            rotate.transform.localEulerAngles = rot;
+
+        }
+        else
+        {
+            angle = 0f;
+        }
         
 
 
 
-        if (h != 0)
-        {
-            transform.Rotate(Vector3.up * Time.deltaTime * h * shipTurnSpeed);
-            rot = this.transform.forward;
-        }
-        else
-        {
-            //Quaternion newRot = Quaternion.LookRotation(rot);
+        //if (h != 0)
+        //{
+        //    transform.Rotate(Vector3.up * Time.deltaTime * h * shipTurnSpeed);
+        //    rot = this.transform.forward;
+        //}
+        //else
+        //{
+        //    //Quaternion newRot = Quaternion.LookRotation(rot);
 
-            ship.transform.rotation = Quaternion.LookRotation(rot);
+        //    ship.transform.rotation = Quaternion.LookRotation(rot);
 
-            transform.rotation = ship.transform.rotation;
-        }
+        //    transform.rotation = ship.transform.rotation;
+        //}
 
 
 
